b158ea60875e791ed9695f1a4fd9051a19cf9d15
powell956 <powell956@gmail.com>
2019-02-11 01:42:01 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -19,5 +20,18 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  # add edit and update methods here
+  def edit
+  end
+
+  def update
+    @article.update(title: params[:article][:title], description: params[:article ][:description])
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
 end
